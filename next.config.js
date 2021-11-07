018252e36283fd2001d5b1920b408b38f8baf902
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-11-07 09:48:39 -0800

    Upgrade to next12 and convert the jobs page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const withMDX = require('@next/mdx')({
     extension: /\.mdx$/
   })
-  module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'mdx']
-  })
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+  },
+})

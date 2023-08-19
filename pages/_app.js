ae110d726d308c1c68b118469a531b85d92501ed
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2023-08-19 08:48:08 -0400

    Remove plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import Layout, { siteTitle } from "../components/layout";
-import PlausibleProvider from "next-plausible";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="daniel.feldroy.com" enabled="true">
-      <Layout
-        meta={pageProps.markdoc ? pageProps.markdoc.frontmatter : pageProps}
-      >
-        <Component {...pageProps} />
-      </Layout>
-    </PlausibleProvider>
+    <Layout
+      meta={pageProps.markdoc ? pageProps.markdoc.frontmatter : pageProps}
+    >
+      <Component {...pageProps} />
+    </Layout>
   );
 }

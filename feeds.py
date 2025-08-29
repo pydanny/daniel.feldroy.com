e0f0394328595409e17973884bf75661551a6db9
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-29 14:25:44 +0800

    Update feeds

--- feeds.py
+++ feeds.py
@@ -100,7 +100,7 @@ def build_feed(content_tag: str | None = None):
         fg.title(f"{content_tag.capitalize()} posts by Daniel Roy Greenfeld")
         posts = list(filter_posts_by_tag(posts, content_tag))
 
-    for raw in posts[:1]:
+    for raw in posts[:2]:
         add_entry(fg, raw)
 
     if content_tag is not None:

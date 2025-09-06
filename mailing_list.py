2c35f7b52139dbe272989591164ea35b6a1e4ec8
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-09-06 15:25:01 +0800

    Getting basic mailgun operational
    Co-Authored-By: Audrey M. Roy Greenfeld <audrey@feldroy.com>

--- mailing_list.py
+++ mailing_list.py
@@ -1,11 +1,31 @@
 import air
+from os import getenv
 from layouts import Layout
 from models import Subscription, async_session
 from rich import print
 from datetime import datetime
 from sqlmodel import select
+from mailgun.client import Client as MailgunClient
 
 router = air.AirRouter()
+
+if getenv("RAILWAY_PUBLIC_DOMAIN"):
+    # Production setting
+    def print(x):
+        pass
+
+    DOMAIN = "https://feldroy.com"
+else:
+    # local dev
+    from rich import print
+
+    DOMAIN = "localhost:8000"
+
+# mailgun config
+mg_key: str = getenv('MAILGUN_API_KEY', '')
+mg_client: MailgunClient = MailgunClient(auth=("api", mg_key))    
+
+MAILSERVER = getenv('MAILSERVER', 'mailhog')
 
 
 @router.page
@@ -40,6 +60,33 @@ def mailing_list():
 
 
 
+def post_email(to: str, subject: str, text: str, html: str, mailserver: str='mailhog') -> None:
+
+    data = {
+        "from": 'hi@feldroy.com',
+        "to": to,
+        "subject": subject,
+        "text": text,
+        "html": html,
+        "o:tag": "Python test",
+    }    
+    # Messages
+    # POST /<domain>/messages
+    if mailserver == 'mailgun':
+        # do mailgun
+        req = mg_client.messages.create(data=data, domain='mg.feldroy.com')
+        return req
+    else:
+        # do mailhog
+        pass
+
+
+def send_subscription_email(email, initial=True):
+    if initial:
+        post_email(email, subject="A confirmation email", text='', html=air.Tag(air.H1('Another email confirmation')).render(), mailserver='mailgun')
+    else:
+        post_email(email, subject="We're sending another confirmation email ", text='', html=air.Tag(air.H1('Another email confirmation')).render(), mailserver='mailgun')
+
 
 
 async def save_subscription(email):
@@ -52,13 +99,16 @@ async def save_subscription(email):
         subscription = Subscription(email=email)    
         session.add(subscription)
         await session.commit()
+        send_subscription_email(email)
         print('[green bold]subscription saved![/green bold]')
     else:
         # Already in the system, email again, and increment the subscribe attempts by 1
+        # let the user know
         subscription.attempts_to_subscribe += 1
         subscription.updated_at = datetime.now()
         session.add(subscription)
         await session.commit()
+        send_subscription_email(email)
         print("[red bold]In the system already, let's send them the email again[/red bold]") 
     
 
